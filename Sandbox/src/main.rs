833a08f22d8dea07a5df77455d58d6788e5ebb4d
SangHyeok Hong <tkdgur4427@gmail.com>
2021-06-16 02:00:16 +0900

    add debug environment with launch.json and create windows by winit

--- Sandbox/src/main.rs
+++ Sandbox/src/main.rs
@@ -1,3 +1,43 @@
+
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    // env_logger is simple logger that can be configured via environment variables, for use with the logging facade exposed by the 'log' crate
+    env_logger::init();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    /*
+        Closures are functions that can capture the enclosing environment
+        |val| val + x
+
+        * using *move* before vertical pipes force closure to take ownership of captured variables:
+        let contains = move |needle| haystack.contains(needle);
+
+        * the underscore ( _ ) is a reserved identifier in Rust and serves different purposes depdning on the context
+        usually it means that something is ignored
+    */
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => {
+                match event {
+                    WindowEvent::CloseRequested => {
+                        *control_flow = ControlFlow::Exit
+                    },
+                    _ => {}
+                    // end of 'event' with [if window_id == window.id()]
+                }
+            },
+            _ => {}
+            // end of 'event'
+        }
+    });
 }
