bfc5480f1444097a09b26c3e18fb5ed4771134e3
SangHyeok Hong <tkdgur4427@gmail.com>
2021-06-18 00:36:56 +0900

    finsihed to create swap_chain

--- Sandbox/src/main.rs
+++ Sandbox/src/main.rs
@@ -1,3 +1,4 @@
+use std::iter;
 
 use winit::{
     event::*,
@@ -58,14 +59,43 @@ impl State {
             },
             None, // trace path
         ).await.unwrap();
+
+        let sc_desc = wgpu::SwapChainDescriptor {
+            // the usage of the swap chain; only supported usage is 'RENDER_ATTACHMENT'
+            // RENDER_ATTACHMENT specifies that the textures will be used to write to the screen
+            usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
+            // the texture format of the swap chain; the only formats that are guaranteed are 'Bgra8Unorm' and 'Bgra8UnormSrgb'
+            format: adapter.get_swap_chain_preferred_format(&surface).unwrap(),
+            // width/height of the swap chain
+            width: size.width,
+            height: size.height,
+            // presentation mode of the swap chain; FIFO is the only guaranteed to be supported, through,
+            // other formats will automatically fall back to FIFO
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+        Self {
+            surface,
+            device,
+            queue,
+            sc_desc,
+            swap_chain,
+            size,
+        }
     }
 
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-
+        self.size = new_size;
+        self.sc_desc.width = new_size.width;
+        self.sc_desc.height = new_size.height;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
     }
 
+    #[allow(unused_variables)]
     fn input(&mut self, event: &WindowEvent) -> bool {
-
+        false
     }
 
     fn update(&mut self) {
@@ -73,7 +103,44 @@ impl State {
     }
 
     fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
+        
+        // we need to get a frame to render to; this will include a wgpu::Texture and wgpu::TextureView that will hold the actual image we're drawing to
+        let frame = self.swap_chain.get_current_frame()?.output;
 
+        // to create CommandEncoder to create the actual commands to send to the gpu
+        // * most modern graphics frameworks expect commands to be stored in a command buffer before being sent to the gpu
+        // * the 'encoder' builds a command buffer that we can then send to the gpu
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+
+        {
+            // get to clearing the screen
+            // * need to use the 'encoder' to create 'RenderPass' to do the actual drawing
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[wgpu::RenderPassColorAttachment {
+                    view: &frame.view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    }
+                }],
+                depth_stencil_attachment: None,
+            });
+        }
+
+        // submit will accept anything that implements IntoIter
+        // std::iter::once - creates an iterator that yields an element exactly once
+        self.queue.submit(iter::once(encoder.finish()));
+
+        Ok(())
     }
 }
 
@@ -83,6 +150,11 @@ fn main() {
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    use futures::executor::block_on;
+
+    // since main can't be async, we're going to need to block
+    let mut state: State = block_on(State::new(&window));
 
     /*
         Closures are functions that can capture the enclosing environment
@@ -100,14 +172,47 @@ fn main() {
                 ref event,
                 window_id,
             } if window_id == window.id() => {
-                match event {
-                    WindowEvent::CloseRequested => {
-                        *control_flow = ControlFlow::Exit
-                    },
-                    _ => {}
-                    // end of 'event' with [if window_id == window.id()]
+                if !state.input(event) {
+                    match event {
+                        WindowEvent::CloseRequested => {
+                            *control_flow = ControlFlow::Exit
+                        },
+                        WindowEvent::KeyboardInput { input, .. } => match input {
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            } => *control_flow = ControlFlow::Exit,
+                            _ => {}
+                        },
+                        WindowEvent::Resized(physical_size) => {
+                            state.resize(*physical_size);
+                        },
+                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                            state.resize(**new_inner_size);
+                        },
+                        _ => {}
+                        // end of 'event' with [if window_id == window.id()]
+                    }
+                }                
+            },
+            Event::RedrawRequested(_) => {
+                state.update();
+                match state.render() {
+                    Ok(_) => {},
+                    // recreate the swap_chain if lost
+                    Err(wgpu::SwapChainError::Lost) => state.resize(state.size),
+                    // the system is out of memory, we should probably quit
+                    Err(wgpu::SwapChainError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                    // all other errors (Outdated, Timeout) should be resolved by the next frame
+                    Err(e) => eprintln!("{:?}", e),
                 }
             },
+            Event::MainEventsCleared => {
+                // RedrawRequested will only trigger once, unless we manually
+                // request it
+                window.request_redraw();
+            }
             _ => {}
             // end of 'event'
         }
